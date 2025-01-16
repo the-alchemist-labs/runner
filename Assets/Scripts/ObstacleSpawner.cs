2667e220f4906d9f91e55778b9b97aaa445b1b99
Sol Elan <solipod8@gmail.com>
2025-01-16 22:26:50 +0200

    Add Obstacles

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,16 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject obstaclePrefab;
-
-    private int _obstacleSpawnCount = 0;
+    [SerializeField] private GameObject[] obstaclePrefabs;
+    [SerializeField] private Transform obstacleContainer;
+    [SerializeField] private float obstacleSpawnInterval = 1f;
+    [SerializeField] private float spawnWidth = 4f;
+    
     void Start()
     {
-        while (_obstacleSpawnCount < 5)
+        StartCoroutine(SpawnObstacleRoutine());
+    }
+
+    private IEnumerator SpawnObstacleRoutine()
+    {
+        while (true)
         {
-            _obstacleSpawnCount++;
-            Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
+            GameObject obstacle = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
+            yield return new WaitForSeconds(obstacleSpawnInterval);
+            Instantiate(obstacle, spawnPosition, Random.rotation, obstacleContainer);
         }
     }
 }
